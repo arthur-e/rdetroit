59eaf48c66390908784612da899554c1e89e4932
K. Arthur Endsley <kaendsle@mtu.edu>
2014-12-02 12:00:11 -0500

    Beginning work on 2011 prediction

--- DetroitCA.R
+++ DetroitCA.R
@@ -6,8 +6,46 @@ library(bnlearn)
 setwd('/usr/local/dev/rdetroit/')
 options(stringsAsFactors=FALSE)
 
-###################
-# Data preparation
+#####################################
+# Data preparation and factorization
+
+file.loc <- '/home/arthur/Workspace/TermProject/'
+
+# Load census measures and the 30-m land cover data
+require(raster)
+load(file='rda/spatialMeasures.rda')
+
+# Recreation and outdoor areas
+rec.area.dist <- raster::raster(paste0(file.loc,
+                                       'ancillary/rec+outdoor_nad83_prox_cut.tiff'))
+
+require(plyr)
+attr2011 <- subset(attr2011, select=c('med.hhold.income', 'male.pop'))
+
+training <- as.data.frame(attr2011)
+training$rec.area.proximity=as.data.frame(rec.area.dist)[1]
+training$med.hhold.income <- as.numeric(training$med.hhold.income)
+
+require(bnlearn)
+training.discrete <- bnlearn::discretize(na.omit(training),
+                                         breaks=rep(2, dim(training)[2]),
+                                         method='quantile')
+
+##############
+# Aggregation
+
+require(raster)
+rast2001 <- raster::raster(paste0(file.loc, 'nlcd2001_nad83.tif'))
+rast2006 <- raster::raster(paste0(file.loc, 'nlcd2006_nad83.tif'))
+rast2011 <- raster::raster(paste0(file.loc, 'nlcd2011_nad83.tif'))
+reclass.matrix <- matrix(c(c(0,10,0), c(10,11,NA), c(12,20,0),
+                           c(20,23,1), c(23,24,2), c(24,99,0)),
+                         byrow=TRUE, ncol=3)
+dev2001 <- raster::reclassify(rast2001, reclass.matrix, right=TRUE) # Intervals closed on right
+dev2006 <- raster::reclassify(rast2006, reclass.matrix, right=TRUE)
+dev2011 <- raster::reclassify(rast2011, reclass.matrix, right=TRUE)
+
+remove(rast2001, rast2006, rast2011)
 
 file.loc <- '/home/arthur/Workspace/TermProject/'
 counties <- readOGR(paste0(file.loc, 'ancillary/co26_d00_select_nad83.shp'),
@@ -17,7 +55,156 @@ counties <- spTransform(counties, crs(dev2001))
 dev2001 <- aggregate(dev2001, fact=10, fun=modal)
 dev2006 <- aggregate(dev2006, fact=10, fun=modal)
 dev2011 <- aggregate(dev2011, fact=10, fun=modal)
-save(dev2001, dev2006, dev2011, file='rda/caAggregates.rda')
 
-load(file='rda/spatialMeasures.rda')
-load(file='rda/caAggregates.rda') # Replace dev2001 and dev2006 with aggregated
+save(dev2001, dev2006, dev2011, file='rda/caAggregates.rda')
+load(file='rda/caAggregates.rda') # Replace 30-m land cover data with aggregated data
+rec.area.dist <- resample(rec.area.dist, dev2001)
+
+##############
+# Rasterizing
+
+vars <- c('med.hhold.income', 'male.pop')
+layers <- as.list(1:length(vars))
+names(layers) <- vars
+layers$rec.area.proximity <- rec.area.dist
+
+# This is key! This is where the census attributes are set
+for (var in vars) {
+  layers[var] <- rasterize(attr2011, dev2011, var) # 2011 census
+}
+
+###########################
+# Masking and discretizing
+
+# This is key! This is the "old" land cover
+layers$old <- raster::mask(dev2006, layers$male.pop, maskvalue=NA) # 2006 land cover
+layers$rec.area.proximity <- raster::mask(layers$rec.area.proximity,
+                                          layers$male.pop, maskvalue=NA)
+
+save(layers, file='rda/predictionLayers2011.rda')
+load(file='rda/predictionLayers2011.rda')
+
+###############
+# Discretizing
+
+# Create a reclass matrix from the levels
+vars <- c('rec.area.proximity', 'med.hhold.income', 'male.pop')
+for (var in vars) {
+  # Split apart e.g. "(20.1,190]"
+  reclass.matrix <- sapply(levels(training.sample[,var]),
+                           function (s) as.numeric(unlist(strsplit(gsub('\\[|\\]|\\(|\\)',
+                                                                        '', s), ','))))
+  reclass.matrix[1] <- 0
+  layers[var] <- raster::reclassify(get(var, layers),
+                                    cbind(reclass.matrix, c(0, 1)),
+                                    right=TRUE) # Intervals closed on right
+}
+
+###########
+# Stacking
+
+layers <- stack(layers)
+
+# TODO An improvement would be to have the levels of the layers be in [0, 1, 2, ...]
+layer.levels <- list(rec.area.proximity=levels(training.sample$rec.area.proximity),
+                     med.hhold.income=levels(training.sample$med.hhold.income),
+                     male.pop=levels(training.sample$male.pop))
+
+save(layers, layer.levels, file='rda/layerStack.rda')
+load(file='rda/layerStack.rda')
+
+# Clean-up
+remove(var, vars, rec.area.dist, roads.dist, reclass.matrix)
+
+####################
+# Utility functions
+
+# TODO An improvement would be to have the levels of the layers be in [0, 1, 2, ...]
+layer.levels <- list(rec.area.proximity=levels(training.sample$rec.area.proximity),
+                     med.hhold.income=levels(training.sample$med.hhold.income),
+                     male.pop=levels(training.sample$male.pop))
+
+# A function to update the posterior probability distribution with evidence
+updateNetwork <- function (jtree, states) {
+  # Do not do anything if the input data are all NA
+  if (all(is.na(states))) {
+    return(jtree)
+  }
+
+  evidence <- transform(states,
+                        med.hhold.income=layer.levels$med.hhold.income[med.hhold.income + 1],
+                        male.pop=layer.levels$male.pop[male.pop + 1],
+                        rec.area.proximity=layer.levels$rec.area.proximity[rec.area.proximity + 1],
+                        old=as.character(old))
+
+  for (i in seq(1, dim(evidence)[1])) {
+    jtree <- setEvidence(jtree, nodes=names(evidence), nslist=mapply(list, evidence[i,]))
+  }
+
+  jtree
+}
+
+# A function to choose outcomes, one at a time, with the same probability as the given posterior distribution
+chooseOutcome <- function (posterior) {
+  posterior <- sort(posterior)
+
+  # Sort the posterior probabilties by factors, e.g. "1=0.56,0=0.44" becomes "0=0.44,1=0.56"
+  post <- numeric()
+  for (i in seq.int(1, length(posterior))) {
+    post[i] <- posterior[as.character(i - 1)]
+  }
+
+  # Generate a vector of probability thresholds e.g. [0.0, 0.44] for transitions to [0, 1]; upper bound of p=1.0 is implied.
+  prob <- rep(0, length(post))
+  for (i in seq.int(length(post) - 1, 1, by=-1)) {
+    j <- length(post) - i
+    prob <- prob + c(rep(0, j), post[(j-1):(length(post)-j)])
+  }
+
+  # Generate a random uniform deviate on [0, 1] to determine which factor to output
+  r <- runif(1)
+  for (i in seq.int(0, length(prob) - 2)) {
+    if (r < prob[i + 2]) {
+      return(i) # p < threshold in e.g. [0, 0.44]? Output that factor
+    }
+  }
+
+  (length(prob) - 1) # p < implied upper bound of 1.0? Output last factor
+}
+
+############################
+# Simulation and prediction
+
+load(file='rda/graphs.rda')
+load(file='rda/layerStack.rda')
+
+require(gRain)
+
+# We use the junction tree algorithm to create an independence network that we can query
+prior.expert <- compile(as.grain(fit.expert))
+prior.mmhc <- compile(as.grain(fit.mmhc))
+
+output.expert.2011 <- stackApply(layers, rep(1, length(names(layers))), function (r, ...) {
+  chooseOutcome(querygrain(updateNetwork(prior.expert, as.data.frame(t(r))),
+                           nodes='new')$new)
+})
+
+output.mmhc.2011 <- stackApply(layers, rep(1, length(names(layers))), function (r, ...) {
+  chooseOutcome(querygrain(updateNetwork(prior.mmhc, as.data.frame(t(r))),
+                           nodes='new')$new)
+})
+
+output.mmhc.2011 <- mask(output.mmhc.2011, layers$male.pop, maskvalue=NA)
+output.expert.2011 <- mask(output.expert.2011, layers$male.pop, maskvalue=NA)
+
+save(output.mmhc.2006, output.expert.2006, file='rda/outputs2006.rda')
+load(file='rda/outputs2006.rda')
+
+
+
+
+
+
+
+
+
